5cae72db46c605d51ce2d5feebfa9ba5899e657a
Ivan Khrulev <subz07@bk.ru>
2019-11-17 19:41:10 +0500

    Forms

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,6 +4,7 @@ import Dishdetail from './DishdetailComponent';
 import Home from './HomeComponent';
 import ContactUs from './ContactComponent';
 import AboutUs from './AboutComponent';
+import Reservation from './ReservationComponent';
 
 import { View, Platform, Text, ScrollView, Image, StyleSheet } from 'react-native';
 import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
@@ -61,6 +62,23 @@ const MenuNavigator = createStackNavigator({
       headerTintStyle: '#fff',
       headerTitleStyle: { color: '#fff' }
   }
+})
+
+const ReservationNavigator = createStackNavigator({
+  Reservation: { screen: Reservation }
+}, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+        backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+        color: "#fff"            
+    },
+    headerTintColor: "#fff",
+    headerLeft: <Icon name="menu" size={24}
+      iconStyle={{ color: 'white' }} 
+      onPress={ () => navigation.navigate('DrawerToggle') } />    
+  })
 })
 
 const ContactUsNavigator = createStackNavigator({
@@ -164,6 +182,21 @@ const MainNavigator = createDrawerNavigator({
             color={tintColor}
           />)
       }, 
+    },
+    Reservation:
+    { screen: ReservationNavigator,
+      navigationOptions: {
+        title: 'Reserve Table',
+        drawerLabel: 'Reserve Table',
+        drawerIcon: ({ tintColor, focused }) => (
+          <Icon
+            name='cutlery'
+            type='font-awesome'            
+            size={24}
+            iconStyle={{ color: tintColor }}
+          />
+        ),
+      }
     }
 }, {
   contentComponent: CustomDrawerContentComponent,
--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,2 +1,3 @@
 export const baseUrl = 'http://192.168.100.2:3001/'
 //export const baseUrl = 'http://localhost:3001/'
+//export const baseUrl = 'http://l3-qp6.anonymous.rest.exp.direct:3001/'
