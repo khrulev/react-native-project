95265e4464c950ec0a49b49b724340e76015383f
Ivan Khrulev <subz07@bk.ru>
2019-12-01 01:25:04 +0500

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -3,6 +3,8 @@ import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Alert } fro
 import { Card } from 'react-native-elements';
 import DatePicker from 'react-native-datepicker'
 import * as Animatable from 'react-native-animatable';
+import { Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
 
 class Reservation extends Component {
 
@@ -29,6 +31,32 @@ class Reservation extends Component {
     //     // console.log(JSON.stringify(this.state));
     //     this.toggleModal();
     // }
+    async obtainNotificationPermission() {
+        let permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notifications');
+            }
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(date) {
+        await this.obtainNotificationPermission();
+        Notifications.presentLocalNotificationAsync({
+            title: 'Your Reservation',
+            body: 'Reservation for '+ date + ' requested',
+            ios: {
+                sound: true
+            },
+            android: {
+                sound: true,
+                vibrate: true,
+                color: '#512DA8'
+            }
+        });
+    }
 
     resetForm() {
         this.setState({
@@ -120,7 +148,10 @@ class Reservation extends Component {
                                  'Date and Time: ' + this.state.date ,
                                 [
                                 {text: 'Cancel', onPress: () => {this.resetForm()}},
-                                {text: 'OK', onPress: () => {this.resetForm()}},
+                                {text: 'OK', onPress: () => {
+                                    this.presentLocalNotification(this.state.date);
+                                    this.resetForm()}
+                                },
                                 ],
                                 { cancelable: false }
                             );}
